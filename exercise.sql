481c2779d19b6a2456303de94ecee8c836e46bc7
et4321 <54795466+et4321@users.noreply.github.com>
2020-03-18 13:46:50 +0100

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,3 +1,11 @@
     USE netland;
 
-    SELECT title FROM series;
+   
+    SELECT title, has_won_awards FROM series WHERE has_won_awards >0;
+    SELECT title, rating FROM series WHERE rating >5;
+    SELECT title, country FROM series WHERE country = 'NL';
+    SELECT title, seasons FROM series WHERE seasons <5;
+    SELECT title, rating FROM series  WHERE rating = (SELECT MAX(rating) FROM series);
+    SELECT title, seasons FROM series WHERE seasons >3 AND seasons <20;
+    SELECT title FROM series order by 'th';
+    SELECT title, seasons FROM series WHERE seasons != 3;
