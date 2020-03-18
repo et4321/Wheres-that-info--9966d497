ab9dd750e6ffb522eb3794a62389fe9a215daf3d
et4321 <54795466+et4321@users.noreply.github.com>
2020-03-18 14:09:40 +0100

    Uitwerking van de exercise

--- queries.sql
+++ queries.sql
@@ -1,11 +1,6 @@
     USE netland;
 
-   
-    SELECT title, has_won_awards FROM series WHERE has_won_awards >0;
-    SELECT title, rating FROM series WHERE rating >5;
-    SELECT title, country FROM series WHERE country = 'NL';
-    SELECT title, seasons FROM series WHERE seasons <5;
-    SELECT title, rating FROM series  WHERE rating = (SELECT MAX(rating) FROM series);
-    SELECT title, seasons FROM series WHERE seasons >3 AND seasons <20;
-    SELECT title FROM series order by 'th';
-    SELECT title, seasons FROM series WHERE seasons != 3;
+
+    SELECT title, rating FROM series WHERE rating >2.5 ORDER BY rating DESC;
+    SELECT title, seasons FROM series WHERE seasons <5 ORDER BY seasons DESC;
+    SELECT title, seasons, country FROM series WHERE seasons <3 AND seasons >20 ORDER BY seasons DESC , country DESC;
